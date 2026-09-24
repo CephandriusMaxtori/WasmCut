efe5b08c8c4d83c2774c52bcdd5f539941dfcce5
Hoid <ngbcoder@gmail.com>
2026-09-24 11:31:05 -0400

    Ignore Emscripten cache in lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["build/**", "dist/**", "node_modules/**", "web/generated/**", "web/public/**"]
+    ignores: ["build/**", "dist/**", "emsdk-cache/**", "node_modules/**", "web/generated/**", "web/public/**"]
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
